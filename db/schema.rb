31ffe36ca5b15a281221bdb4200b5ca7e2b16a8d
Alessandrofig <amfigueroa2@miuandes.cl>
2024-06-21 17:09:42 -0400

    Actualización

--- db/schema.rb
+++ db/schema.rb
@@ -51,11 +51,20 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_27_170645) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
+  create_table "tweets", force: :cascade do |t|
+    t.string "content", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
+    t.string "password", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -66,4 +75,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_27_170645) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "tweets", "users"
 end
